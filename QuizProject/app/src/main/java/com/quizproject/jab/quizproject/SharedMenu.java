86c134591b0b4df568567287f4fbba1b7214e657
CD <CD@CDD.CO>
2018-08-17 15:10:08 -0400

    Added menu functionality

--- QuizProject/app/src/main/java/com/quizproject/jab/quizproject/SharedMenu.java
+++ QuizProject/app/src/main/java/com/quizproject/jab/quizproject/SharedMenu.java
@@ -1,5 +1,6 @@
 package com.quizproject.jab.quizproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,13 +19,13 @@ public class SharedMenu extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.menuItemNewQuiz:
-                //startActivity(new Intent(this, About.class));
+                startActivity(new Intent(this, OptionsSelectActivity.class));
                 return true;
             case R.id.menuItemViewRecentResults:
-                //startActivity(new Intent(this, Help.class));
+                startActivity(new Intent(this, AllResultsActivity.class));
                 return true;
             case R.id.menuItemSignOut:
-                //startActivity(new Intent(this, Help.class));
+                startActivity(new Intent(this, MainActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
